2788296e15c1dbfbc4e1b4608ae6f5f2a9cb6216
Nic <nvogler@umich.edu>
2017-10-21 06:53:33 +0000

    ddl fix

--- hive_base_ddl.sql
+++ hive_base_ddl.sql
@@ -1,17 +1,18 @@
-DROP TABLE Measures
-CREATE EXTERNAL TABLE Measures (
-	MeasureName,
-	MeasureID,
-	MeasureStartQuarter,
-	MeasureStartDate,
-	MeasureEndQuarter,
-	MeasureEndDate
-	)
+DROP TABLE Measures;
+CREATE EXTERNAL TABLE Measures
+(
+	MeasureName STRING,
+	MeasureID INT,
+	MeasureStartQuarter STRING,
+	MeasureStartDate STRING,
+	MeasureEndQuarter STRING,
+	MeasureEndDate STRING
+)
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 	WITH SERDEPROPERTIES (
 		"seperatorChar" = ",",
 		"quoteChar" = '"',
 		"escapeChar" = '\\'
 		)
-		STORED AS Measures.csv
-		LOCATION '/user/w205/hospital_compare';
+	STORED AS TEXTFILE
+	LOCATION '/user/w205/hospital_compare/Measure.csv';
