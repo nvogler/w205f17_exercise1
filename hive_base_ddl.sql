a95eca7942fcc0feaf62a469ede0adec2c4f32e5
nvogler <nvogler@umich.edu>
2017-10-21 03:19:12 -0400

    Updated hive ddl

--- hive_base_ddl.sql
+++ hive_base_ddl.sql
@@ -1,3 +1,132 @@
+DROP TABLE Hospitals;
+CREATE EXTERNAL TABLE Hospitals
+(
+	ProviderID INT,
+	HospitalName STRING,
+	Address STRING,
+	City STRING,
+	State STRING,
+	ZipCode INT,
+	CountyName STRING,
+	PhoneNumber STRING,
+	HospitalType STRING,
+	HospitalOwnership STRING,
+	EmergencyServices BOOLEAN
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+	WITH SERDEPROPERTIES (
+		"seperatorChar" = ",",
+		"quoteChar" = '"',
+		"escapeChar" = '\\'
+		)
+	STORED AS TEXTFILE
+	LOCATION '/user/w205/hospital_compare/hospitals.csv';
+
+DROP TABLE SurveyResponses;
+CREATE EXTERNAL TABLE SurveyResponses
+(
+	ProviderID INT,
+	HospitalName STRING,
+	Address STRING,
+	City STRING,
+	State STRING,
+	ZipCode INT,
+	CountyName STRING,
+	QuestionA STRING,
+	QuestionB STRING,
+	QuestionC STRING,
+	QuestionD STRING,
+	QuestionE STRING,
+	QuestionF STRING,
+	QuestionG STRING,
+	QuestionH STRING,
+	QuestionI STRING,
+	QuestionJ STRING,
+	QuestionK STRING,
+	QuestionL STRING,
+	QuestionM STRING,
+	QuestionN STRING,
+	QuestionO STRING,
+	QuestionP STRING,
+	QuestionQ STRING,
+	QuestionR STRING,
+	QuestionS STRING,
+	QuestionT STRING,
+	QuestionU STRING,
+	QuestionV STRING,
+	QuestionW STRING,
+	QuestionX STRING,
+	QuestionY STRING,
+	baseScore INT,
+	ConsistencyScore INT,
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+	WITH SERDEPROPERTIES (
+		"seperatorChar" = ",",
+		"quoteChar" = '"',
+		"escapeChar" = '\\'
+		)
+	STORED AS TEXTFILE
+	LOCATION '/user/w205/hospital_compare/surveys_responses.csv';
+
+DROP TABLE Readmissions;
+CREATE EXTERNAL TABLE Readmissions
+(
+	ProviderID INT,
+	HospitalName STRING,
+	Address STRING,
+	City STRING,
+	State STRING,
+	ZipCode INT,
+	CountyName STRING,
+	PhoneNumber STRING,
+	MeasureName STRING,
+	MeasureID INT,
+	Denominator STRING,
+	Score STRING,
+	LowerEstimate STRING,
+	HigherEstimate STRING,
+	Footnote STRING,
+	MeasureStartDate STRING,
+	MeasureEndDate STRING
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+	WITH SERDEPROPERTIES (
+		"seperatorChar" = ",",
+		"quoteChar" = '"',
+		"escapeChar" = '\\'
+		)
+	STORED AS TEXTFILE
+	LOCATION '/user/w205/hospital_compare/Measure.csv';
+
+DROP TABLE EffectiveCare;
+CREATE EXTERNAL TABLE EffectiveCare
+(
+	ProviderID INT,
+	HospitalName STRING,
+	Address STRING,
+	City STRING,
+	State STRING,
+	ZipCode INT,
+	CountyName STRING,
+	Condition STRING,
+	MeasureID INT,
+	MeasureName STRING,
+	Score STRING,
+	Sample STRING,
+	Footnote STRING,
+	MeasureStartDate STRING,
+	MeasureEndDate STRING
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+	WITH SERDEPROPERTIES (
+		"seperatorChar" = ",",
+		"quoteChar" = '"',
+		"escapeChar" = '\\'
+		)
+	STORED AS TEXTFILE
+	LOCATION '/user/w205/hospital_compare/Measure.csv';
+	
 DROP TABLE Measures;
 CREATE EXTERNAL TABLE Measures
 (
