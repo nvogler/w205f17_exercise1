da199fc29f274fbb85c90b54714e7e26f6f07a89
nvogler <nvogler@umich.edu>
2017-10-31 13:51:15 -0400

    Added remaining investigation files

---
+++ investigations/best_hospitals/best_hospitals.sql
@@ -0,0 +1,40 @@
+-- Desired output: ProviderID, average of (average score - standard deviation), aggregate, average, and standard deviation for each measure
+SELECT ProviderID, avg(AMS) - avg(VAR) AS tot, avg(AMS) AS avrg,  stddev_samp(AMS) as avar, sum(AMS) AS sumavg, avg(divva), avg(divav)
+FROM
+	(
+		-- Calculate standard deviation and average score for each providerid, measureid tuple
+		SELECT providerid, measureid, stddev_samp(score) AS VAR, avg(score) AS AMS
+		FROM
+			-- Joining all procedures and their scores for a given providerid and filtering providerids associated with less than 10 procedures
+			(	
+				-- UNION ALL is used to concatenate data on all the procedures during an initial visit, readmission, or death.
+				SELECT providerid, measureid, score
+				FROM readmissions_f
+				UNION ALL
+				SELECT providerid, measureid, score
+				FROM effective_care_f
+				WHERE providerid IS NOT NULL
+				AND score IS NOT NULL
+			) AS avgs
+			INNER JOIN
+			(
+			-- Count distinct procedures per providerid 
+			SELECT providerid, COUNT(DISTINCT(measureid)) AS variety
+			FROM 
+				(
+					-- UNION ALL is used to concatenate data on all the procedures during an initial visit, readmission, or death.
+					SELECT providerid, measureid, score
+					FROM readmissions_f
+					UNION ALL
+					SELECT providerid, measureid, score
+					FROM effective_care_f
+				) unionResult
+			GROUP BY ProviderID
+			) as cnt
+			ON cnt.providerid = avgs.providerid 
+		WHERE cnt.variety > 10
+		-- Grouping by providerid, measureid tuple before being passed up for final calculations
+		GROUP BY avgs.providerid, avgs.measureid
+	) subResult
+GROUP BY ProviderID
+ORDER BY tot DESC LIMIT 10;
